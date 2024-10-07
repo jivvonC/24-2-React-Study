eda642ca641188ace8ee142ed796a26dc399fff7
Jiwon Chon <jiwonchon07@gmail.com>
2024-10-08 00:57:13 +0900

    4주차실습

---
+++ w4/pokemon/src/theme.js
@@ -0,0 +1,21 @@
+export const LightTheme = {
+    gridcontainer: {
+        background: "#ffffff"
+    },
+    griditem: {
+        background: "#d3d3d3",
+        textcolor: "#000000",
+        bordercolor: "#a9a9a9"
+    }
+  };
+  
+  export const DarkTheme = {
+    gridcontainer: {
+        background: "#202020"
+    },
+    griditem: {
+        background: "#636363",
+        textcolor: "#ffffff",
+        bordercolor: "#ffffff"
+    }
+  };
