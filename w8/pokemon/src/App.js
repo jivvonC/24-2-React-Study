5b962dabc63c6e2865a70df6b5e890ca7bffafce
Jiwon Chon <jiwonchon07@gmail.com>
2024-11-23 22:39:58 +0900

    배포용 수정6

--- w8/pokemon/src/App.js
+++ w8/pokemon/src/App.js
@@ -63,14 +63,14 @@ function Home(){
             });
           }
           setPokemonData(allPokemonData); // 상태 업데이트
-          console.log('fetch끝')
-          localStorage.setItem('pokemonData', JSON.stringify(pokemonData));
+          // console.log('fetch끝')
+          // localStorage.setItem('pokemonData', JSON.stringify(pokemonData));
   
         } catch (err) {
           console.log(err);
         }
       }
-        //fetchPokemon();
+        fetchPokemon();
       //   async function handleStorage(){
       //     if(localStorage.getItem('pokemonData')== '[]'){
       //       await fetchPokemon();
@@ -80,16 +80,16 @@ function Home(){
       //   }
       //   handleStorage();
   
-      if(localStorage.getItem('pokemonData')== '[]'){
-        console.log('if')
-        fetchPokemon(); 
-        console.log('done')}
+      // if(localStorage.getItem('pokemonData')== '[]'){
+      //   console.log('if')
+      //   fetchPokemon(); 
+      //   console.log('done')}
 
     }, []);
     
     
-     let pokepoke = localStorage.getItem('pokemonData')
-     pokemonData= JSON.parse(pokepoke)
+    //  let pokepoke = localStorage.getItem('pokemonData')
+    //  pokemonData= JSON.parse(pokepoke)
 
     useEffect(()=>{
       if (localStorage.getItem('theme') == null){
